7adfa01fd2e9826e9ff5e4f7da16f3a50b5c973a
tony albanese <albanesetr@gmail.com>
2013-08-06 14:37:07 -0700

    bandaid for migration draft

--- db/migrate/20130724045229_add_draft_to_posts.rb
+++ db/migrate/20130724045229_add_draft_to_posts.rb
@@ -1,6 +1,7 @@
 class AddDraftToPosts < ActiveRecord::Migration
-  def self.up
-    change_table :posts do |t|
-      t.boolean :draft, default: true
+  def up
+  end
+
+  def down
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130724052156) do
+ActiveRecord::Schema.define(:version => 20130806213518) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
